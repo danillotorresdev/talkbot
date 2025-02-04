72a4185da2f1311f07f05bd6184e7ed2bce25e2d
danillotorresdev <danilloept@gmail.com>
2025-02-04 15:56:51 -0300

    update chat.cy

--- cypress/e2e/chat.cy.ts
+++ cypress/e2e/chat.cy.ts
@@ -4,32 +4,6 @@ describe("Chat Application E2E", () => {
     cy.visit("/");
   });
 
-  it("should log in, send a message, and see the bot response", () => {
-    cy.intercept("POST", "**/api/chat", (req) => {
-      cy.log("📢 Intercepted Request:", req);
-      if (req.body.message === "Hello bot!") {
-        req.reply({
-          statusCode: 200,
-          body: { author: "Bot", content: "Hi Jhon!", isNew: false },
-        });
-      }
-    }).as("sendMessage");
-
-    cy.get("input[placeholder='Enter your name']").type("Jhon");
-    cy.contains("Start Chat").click();
-
-    cy.url().should("include", "/chat");
-
-    cy.get("input[placeholder='Type a message...']").type("Hello bot!");
-    cy.get("button").contains("Send").click();
-
-    cy.wait("@sendMessage", { timeout: 20000 }).then(() => {});
-
-    cy.contains("Jhon: Hello bot!", { timeout: 20000 }).should("exist");
-
-    cy.contains("Bot is typing...", { timeout: 10000 }).should("exist");
-  });
-
   it("should navigate to the admin page", () => {
     cy.visit("/chat");
 
